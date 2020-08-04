b6d09d0aea47a0d9f83105222aaaf6628b02442c
ecettour <ecettour@emmsa.net>
2020-08-04 20:07:25 -0300

    Adding missing file to previous commit

---
+++ eCommerce.Repository/Repositories/UserRepository.cs
@@ -0,0 +1,12 @@
+﻿using Core.Entities;
+using eCommerce.Infrastructure.Shared;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eCommerce.Infrastructure.Repositories
+{
+    //public class UserRepository : GenericRepository<User>
+    //{
+    //}
+}
