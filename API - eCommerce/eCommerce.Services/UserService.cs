d0c3048201a4c944656fba04901d589379e64cd8
ecettour <ecettour@emmsa.net>
2020-08-09 22:53:38 -0300

    Added AutoMapper Added FluentValidation Added Swagger

--- API - eCommerce/eCommerce.Services/UserService.cs
+++ API - eCommerce/eCommerce.Services/UserService.cs
@@ -1,8 +1,10 @@
 ﻿using Core.Entities;
 using eCommerce.Core.Interfaces.Repositories;
 using eCommerce.Core.Interfaces.Services;
+using eCommerce.Services.Validators;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
@@ -27,6 +29,41 @@ namespace eCommerce.Services
         public async Task<IEnumerable<User>> GetActiveUsers()
         {
             return await _unitOfWork.Users.GetActiveUsers();
+        }
+
+        public User GetById(int id)
+        {
+            return _unitOfWork.Users.GetByID(id);
+        }
+
+        public async Task Update(User user)
+        {
+            _unitOfWork.Users.Update(user);
+            await _unitOfWork.SaveChangesAsync();
+        }
+        public async Task Insert(User user)
+        {
+            var validationResult = await new UserValidator().ValidateAsync(user);
+
+            if (!validationResult.IsValid)
+                throw new ValidationException(validationResult.Errors.First().ErrorMessage); //TODO: Fix error message
+
+            _unitOfWork.Users.Insert(user);
+
+            await _unitOfWork.SaveChangesAsync();
+        }
+
+        public async Task<User> Delete(int id)
+        {
+            var user = _unitOfWork.Users.GetByID(id);
+            if (user == null)
+                throw new ValidationException($"Validation: User with id {id} not found.");
+
+            _unitOfWork.Users.Delete(id);
+
+            await _unitOfWork.SaveChangesAsync();
+
+            return user;
         }
     }
 }
---
+++ API - eCommerce/eCommerce.Services/Validators/UserValidator.cs
@@ -0,0 +1,16 @@
+﻿using Core.Entities;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eCommerce.Services.Validators
+{
+    public class UserValidator : AbstractValidator<User>
+    {
+        public UserValidator()
+        {
+            RuleFor(x => x.Email).Must(x => x.Contains("@")).WithMessage("Email is not valid. Lack of '@'.");
+        }
+    }
+}
